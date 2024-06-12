60cdd483e16b37c5c4c63c9e0b31cf2c629b7016
SuellenBarretos <168095798+SuellenBarretos@users.noreply.github.com>
2024-06-12 11:29:02 -0300

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,29 @@
+var Vermelho= window.document.getElementById('CorV');
+Vermelho.style.color = 'red';
+Vermelho.style.backgroundColor = 'black';  
+
+var Rosa= window.document.getElementById('CorR');
+Rosa.style.color =  'Pink';
+Rosa.style.backgroundColor = 'black';
+
+var divizao= window.document.querySelector('div#div1');
+divizao.style.backgroundColor = 'black';
+
+var pa = window.document.getElementsByTagName('p')[0];
+pa.style.color = 'blue'
+pa.style.backgroundColor = 'black'
+
+var pa = window.document.getElementsByTagName('p')[1];
+pa.style.color = 'purple'
+pa.style.backgroundColor = 'black'
+
+var pa = window.document.getElementsByTagName('p')[2];
+pa.style.color = 'grey'
+pa.style.backgroundColor = 'black'
+
+var pa = window.document.getElementsByTagName('p')[3];
+pa.style.color = 'orange'
+pa.style.backgroundColor = 'black'
+
+var divizao= window.document.querySelector('div#div2')
+divizao.style.backgroundColor = 'black'
